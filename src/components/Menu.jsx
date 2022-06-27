999b506432ff6761fa56eeaa079466eed5423581
Victor Prado <vic.prat182@gmail.com>
2022-06-26 20:42:01 -0500

    preparando todo para producción

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from '@styles/Menu.module.scss';
 
 const Menu = () => {
-	return (
-		<div className={styles.Menu}>
-			<ul>
-				<li>
-					<a href="/" className={styles.title}>My orders</a>
-				</li>
-				<li>
-					<a href="/">My account</a>
-				</li>
-				<li>
-					<a href="/">Sign out</a>
-				</li>
-			</ul>
-		</div >
-	);
-}
+  return (
+    <div className={styles.Menu}>
+      <ul>
+        <li>
+          <Link href="/" className={styles.title}>
+            My orders
+          </Link>
+        </li>
+        <li>
+          <Link href="/">My account</Link>
+        </li>
+        <li>
+          <Link href="/">Sign out</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Menu;
